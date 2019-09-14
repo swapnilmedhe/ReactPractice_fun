ef9a756b17a2ed29458df9e1dc20e8271cf68d81
Swapnil <swapnilme@Cybage.com>
2019-09-14 15:26:20 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,40 @@
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import ReactDOM from 'react-dom'
+// class App extends React.Component {
+   
+//     render(){
+//       <div>
+
+//       </div>
+//     }
+
+// }
+
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello ...</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
+
+
+
+
+
+ 
+
+
+
+
+
+
+
